58f3c241ae90e74184e7298389156115d87ae4a8
Camila Guimarães <38961741+miilacg@users.noreply.github.com>
2021-06-11 14:38:38 -0300

    BOARD: utilização de um loop para fazer os quadrados

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -6,34 +6,48 @@ import './../index.css';
 
 
 export class Board extends React.Component {
-  renderSquare(i) {
+  /**
+   * 
+   * renderSquare(i) {
+    const board = <Square
+                    style={ `square${i}` } 
+                    value={ this.props.squares[i] }
+                    onClick={ () => this.props.onClick(i) }
+                  />  
     return (
-			<Square
-				style={ `square${i}` } 
-				value={ this.props.squares[i] }
-				onClick={ () => this.props.onClick(i) }
-			/>
+			board
 		);
-  }
+  } 
+   
+  */
+ 
+  renderSquare(j) {
+    let board = [];
+
+    for(let i = j; i < (j + 3); i++){
+      board[i] = <Square
+                  style={ `square${i}` } 
+                  value={ this.props.squares[i] }
+                  onClick={ () => this.props.onClick(i) }
+                />
+    }
+    
+    return board;
+  } 
+
 
   render() {
 		return (
       <div>
         <div className="board-row">
           { this.renderSquare(0) }
-          { this.renderSquare(1) }
-          { this.renderSquare(2) }
-        </div>
+        </div> 
         <div className="board-row">
           { this.renderSquare(3) }
-          { this.renderSquare(4) }
-          { this.renderSquare(5) }
         </div>
         <div className="board-row">
           { this.renderSquare(6) }
-          { this.renderSquare(7) }
-          { this.renderSquare(8) }
-        </div>
+        </div>    
       </div>
     );
   }
